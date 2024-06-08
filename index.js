cde8e10f226c1578988f9946a0964e693a1932ff
DangKhoa21 <nldkhoa21@apcs.fitus.edu.vn>
2024-06-08 23:27:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const expressHandlebars = require('express-handlebars');
+
+//config public static folder
+app.use(express.static(__dirname + '/public'));
+
+//config handlebars
+app.engine('hbs', expressHandlebars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    extname: 'hbs',
+    defaultLayout: 'layout',
+}));
+app.set('view engine', 'hbs');
+
+// routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/:page', (req, res) => {
+    res.render(req.params.page);
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
