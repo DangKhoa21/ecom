2e3bb0bfd78e6c09099a797caf88de7721d86e3c
DangKhoa21 <nldkhoa21@apcs.fitus.edu.vn>
2024-07-03 19:31:01 +0700

    product & brand ui

--- public/js/main.js
+++ public/js/main.js
@@ -54,8 +54,8 @@
         margin: 25,
         nav : true,
         navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
+            '<i class="bi bi-arrow-right"></i>',
+            '<i class="bi bi-arrow-left"></i>'
         ],
         responsiveClass: true,
         responsive: {
@@ -88,8 +88,8 @@
         margin: 25,
         nav : true,
         navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
+            '<i class="bi bi-arrow-right"></i>',
+            '<i class="bi bi-arrow-left"></i>'
         ],
         responsiveClass: true,
         responsive: {
@@ -111,6 +111,32 @@
         }
     });
 
+    $(".brand-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 2000,
+        center: false,
+        dots: true,
+        loop: true,
+        margin: 25,
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            },
+            1200:{
+                items:4
+            }
+        }
+    });
 
     // Modal Video
     $(document).ready(function () {
