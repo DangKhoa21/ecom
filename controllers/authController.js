8a43f7feaa6486f851b8ddad0cf383359d35480c
DangKhoa21 <nldkhoa21@apcs.fitus.edu.vn>
2024-07-28 22:16:38 +0700

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,19 +6,19 @@ controller.show = (req, res) => {
     res.render('login-sign-up', { loginMessage: req.flash('loginMessage') });
 }
 
-controller.login = (req, res) => {
+controller.login = (req, res, next) => {
     let keepSignedIn = req.body.keepSignedIn;
     passport.authenticate('local-login', (error, user) => {
         if (error) {
             return next(error);
         }
         if (!user) {
-            return res.redirect('/login-sign-up');
+            return res.redirect('/users/login-sign-up');
         }
         req.logIn(user, (error) => {
             if (error) { return next(error); }
             req.session.cookie.maxAge = keepSignedIn ? (24 * 60 * 60 * 1000) : null;
-            return res.redirect('/account')
+            return res.redirect('/users/account')
         });
     })(req, res, next);
 }
